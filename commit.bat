cbbd5129ad338b34d10eb1615bce1166a2ebdb80
Alexander <alexanderjohnson2011@gmail.com>
2026-03-21 09:34:04 -0700

    Initial commit: La Carreta Catering site

---
+++ commit.bat
@@ -0,0 +1,3 @@
+cd C:\Users\alexa\Downloads\lacarreta-site
+git add .
+git commit -m "Initial commit: La Carreta Catering site"
---
+++ setup.bat
@@ -0,0 +1,5 @@
+cd C:\Users\alexa\Downloads\lacarreta-site
+git init
+git add .
+git commit -m "Initial commit: La Carreta Catering site"
+gh repo create alexanderjohnson2011-cpu/lacarretacatering --public --source=. --remote=origin --push
